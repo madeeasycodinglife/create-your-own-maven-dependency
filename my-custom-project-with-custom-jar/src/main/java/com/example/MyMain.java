b08a9da51e4bbd9ef28c584806baad178f838572
Pabitra Bera <126568738+madeeasycodinglife@users.noreply.github.com>
2023-07-03 09:39:25 +0530

    Add files via upload

---
+++ my-custom-project-with-custom-jar/src/main/java/com/example/MyMain.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import com.madeeasy.Main;
+
+public class MyMain {
+    public static void main(String[] args) {
+        Main main = new Main();
+        Integer result = main.add(1, 2);
+        System.out.println("result = " + result);
+    }
+}
